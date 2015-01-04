dae82a5ede51a286f92a1a0f352be02cdfcb5a73
Kali Kaneko <kali@leap.se>
2015-01-04 03:37:18 -0400

    tests for mail.mail module: Message

--- src/leap/mail/imap/messageparts.py
+++ src/leap/mail/imap/messageparts.py
@@ -17,19 +17,12 @@
 MessagePart implementation. Used from IMAPMessage.
 """
 import logging
-import StringIO
 
 from zope.interface import implements
 from twisted.mail import imap4
 
-from leap.common.decorators import memoized_method
-from leap.common.mail import get_email_charset
-from leap.mail.utils import empty, find_charset
-
 logger = logging.getLogger(__name__)
 
-
-# TODO -- use messagewrapper
 
 class MessagePart(object):
     """
@@ -43,15 +36,9 @@ class MessagePart(object):
 
     implements(imap4.IMessagePart)
 
-    def __init__(self, soledad, part_map):
+    def __init__(self, msgpart):
         """
         Initializes the MessagePart.
-
-        :param soledad: Soledad instance.
-        :type soledad: Soledad
-        :param part_map: a dictionary containing the parts map for this
-                         message
-        :type part_map: dict
         """
         # TODO
         # It would be good to pass the uid/mailbox also
@@ -65,8 +52,7 @@ class MessagePart(object):
         # to gather the results of the deferred operations
         # to signal the operation is complete.
 
-        self._soledad = soledad
-        self._pmap = part_map
+        self.msgpart = msgpart
 
     def getSize(self):
         """
@@ -75,13 +61,7 @@ class MessagePart(object):
         :return: size of the message, in octets
         :rtype: int
         """
-        if empty(self._pmap):
-            return 0
-        size = self._pmap.get('size', None)
-        if size is None:
-            logger.error("Message part cannot find size in the partmap")
-            size = 0
-        return size
+        return self.msgpart.get_size()
 
     def getBodyFile(self):
         """
@@ -90,101 +70,51 @@ class MessagePart(object):
         :return: file-like object opened for reading
         :rtype: StringIO
         """
-        fd = StringIO.StringIO()
-        if not empty(self._pmap):
-            multi = self._pmap.get('multi')
-            if not multi:
-                phash = self._pmap.get("phash", None)
-            else:
-                pmap = self._pmap.get('part_map')
-                first_part = pmap.get('1', None)
-                if not empty(first_part):
-                    phash = first_part['phash']
-                else:
-                    phash = None
+        return self.msgpart.get_body_file()
 
-            if phash is None:
-                logger.warning("Could not find phash for this subpart!")
-                payload = ""
-            else:
-                payload = self._get_payload_from_document_memoized(phash)
-                if empty(payload):
-                    payload = self._get_payload_from_document(phash)
+        #fd = StringIO.StringIO()
+        #if not empty(self._pmap):
+            #multi = self._pmap.get('multi')
+            #if not multi:
+                #phash = self._pmap.get("phash", None)
+            #else:
+                #pmap = self._pmap.get('part_map')
+                #first_part = pmap.get('1', None)
+                #if not empty(first_part):
+                    #phash = first_part['phash']
+                #else:
+                    #phash = None
+#
+            #if phash is None:
+                #logger.warning("Could not find phash for this subpart!")
+                #payload = ""
+            #else:
+                #payload = self._get_payload_from_document_memoized(phash)
+                #if empty(payload):
+                    #payload = self._get_payload_from_document(phash)
+#
+        #else:
+            #logger.warning("Message with no part_map!")
+            #payload = ""
+#
+        #if payload:
+            #content_type = self._get_ctype_from_document(phash)
+            #charset = find_charset(content_type)
+            #if charset is None:
+                #charset = self._get_charset(payload)
+            #try:
+                #if isinstance(payload, unicode):
+                    #payload = payload.encode(charset)
+            #except UnicodeError as exc:
+                #logger.error(
+                    #"Unicode error, using 'replace'. {0!r}".format(exc))
+                #payload = payload.encode(charset, 'replace')
+#
+        #fd.write(payload)
+        #fd.seek(0)
+        #return fd
 
-        else:
-            logger.warning("Message with no part_map!")
-            payload = ""
-
-        if payload:
-            content_type = self._get_ctype_from_document(phash)
-            charset = find_charset(content_type)
-            if charset is None:
-                charset = self._get_charset(payload)
-            try:
-                if isinstance(payload, unicode):
-                    payload = payload.encode(charset)
-            except UnicodeError as exc:
-                logger.error(
-                    "Unicode error, using 'replace'. {0!r}".format(exc))
-                payload = payload.encode(charset, 'replace')
-
-        fd.write(payload)
-        fd.seek(0)
-        return fd
-
-    # TODO should memory-bound this memoize!!!
-    @memoized_method
-    def _get_payload_from_document_memoized(self, phash):
-        """
-        Memoized method call around the regular method, to be able
-        to call the non-memoized method in case we got a None.
-
-        :param phash: the payload hash to retrieve by.
-        :type phash: str or unicode
-        :rtype: str or unicode or None
-        """
-        return self._get_payload_from_document(phash)
-
-    def _get_payload_from_document(self, phash):
-        """
-        Return the message payload from the content document.
-
-        :param phash: the payload hash to retrieve by.
-        :type phash: str or unicode
-        :rtype: str or unicode or None
-        """
-        # XXX use adaptor
-        return NotImplementedError()
-
-    # TODO should memory-bound this memoize!!!
-    @memoized_method
-    def _get_ctype_from_document(self, phash):
-        """
-        Reeturn the content-type from the content document.
-
-        :param phash: the payload hash to retrieve by.
-        :type phash: str or unicode
-        :rtype: str or unicode
-        """
-        # XXX use adaptor
-        return NotImplementedError()
-
-    @memoized_method
-    def _get_charset(self, stuff):
-        # TODO put in a common class with LeapMessage
-        """
-        Gets (guesses?) the charset of a payload.
-
-        :param stuff: the stuff to guess about.
-        :type stuff: str or unicode
-        :return: charset
-        :rtype: unicode
-        """
-        # XXX existential doubt 2. shouldn't we make the scope
-        # of the decorator somewhat more persistent?
-        # ah! yes! and put memory bounds.
-        return get_email_charset(stuff)
-
+    # FIXME ---
     def getHeaders(self, negate, *names):
         """
         Retrieve a group of message headers.
@@ -235,32 +165,10 @@ class MessagePart(object):
         """
         Return True if this message is multipart.
         """
-        if empty(self._pmap):
-            logger.warning("Could not get part map!")
-            return False
-        multi = self._pmap.get("multi", False)
-        return multi
+        return self.msgpart.is_multipart()
 
     def getSubPart(self, part):
         """
         Retrieve a MIME submessage
-
-        :type part: C{int}
-        :param part: The number of the part to retrieve, indexed from 0.
-        :raise IndexError: Raised if the specified part does not exist.
-        :raise TypeError: Raised if this message is not multipart.
-        :rtype: Any object implementing C{IMessagePart}.
-        :return: The specified sub-part.
         """
-        if not self.isMultipart():
-            raise TypeError
-
-        sub_pmap = self._pmap.get("part_map", {})
-        try:
-            part_map = sub_pmap[str(part + 1)]
-        except KeyError:
-            logger.debug("getSubpart for %s: KeyError" % (part,))
-            raise IndexError
-
-        # XXX check for validity
-        return MessagePart(self._soledad, part_map)
+        return MessagePart(self.msgpart.get_subpart(part))
